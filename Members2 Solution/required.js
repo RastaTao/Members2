c6cb8c1ae4f17b1f1b848fe2e07cce04da88c7ec
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2014-05-22 11:52:53 +0200

    Update #5

--- Members2 Solution/required.js
+++ Members2 Solution/required.js
@@ -1,44 +1,42 @@
 
-function customLogin(userName,password)
+var userLogin = function(username, password)
+
+{
+	
+	if(username == "test")
 	{
+		return false; //revert to directory authentication
+	} else {
 		
-		var adminUser = ds.Admin.find("user==:1",userName);
-		var user = ds.Organisation.find("user==:1",userName);
-		var mobile = ds.User.find("phone==:1",userName);
-		
-		if (adminUser) {
-			
+				var adminUser = ds.Admin.find("user==:1",username);
+				var user = ds.Organisation.find("user==:1",username);
+				var mobile = ds.User.find("phone==:1",username);
 				
-				if(adminUser.validatePassword(password))
+				if(adminUser != null){
+											if(adminUser.validatePassword(password))
+											{
+												return {'ID':adminUser.ID,'name':adminUser.user,'fullName':adminUser.user,'belongsTo':['Administrator']};
+										 	}
+									  }
+									 
+				if(user != null)
 				{
-						return {'ID':adminUser.ID,'name':adminUser.user,'fullName':adminUser.user,'belongsTo':['Administrator']};
-						
-				} else {
-						return { error:1, errorMessage:'Password is incorrect'};
-					   }
-			   } 
-		else if(user) {
-			   					if(user.validatePassword(password))
-								{
-									return {'ID':user.ID,'name':user.user,'fullName':user.name,'belongsTo':['Organisation']}; 
-								}else {
-									  return { error:2, errorMessage:'Password is incorrect'};
-									  }
+						if(user.validatePassword(password))
+							{
+								return {'ID':user.ID,'name':user.user,'fullName':user.name,'belongsTo':['Organisation']};
+							}
+				}
+					
+					
+				
+				if(mobile != null)
+				{
+						if(mobile.validatePassword(password))
+							{
+								return {'ID':mobile.ID,'name':mobile.phone,'fullName':mobile.name,'belongsTo':['Mobile']}; 
+							}
+				}					  
+								  
 
-					  }else if(mobile) {
-			   					if(mobile.validatePassword(password))
-								{
-									return {'ID':mobile.ID,'name':mobile.phone,'fullName':mobile.name,'belongsTo':['Mobile']}; 
-								}else {
-									  return { error:3, errorMessage:'Password is incorrect'};
-									  }
-
-					  }else {
-					   return { error:4, errorMessage:'User is incorrect'};
-					        } {
-					   return { error:5, errorMessage:'User is incorrect'};
-					        }
-
-		
-
-	}
+			}
+}
